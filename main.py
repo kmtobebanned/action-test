79d91b9d614a5bf3368c2e8e47f56862a09e9f06
kemalmsi <kemal.maulidiasani@accelbyte.net>
2023-01-13 15:49:29 +0700

    test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import argparse
+import os
 
+DOCUPATH = '/Test/test/'
 def parse_args():
     parser=argparse.ArgumentParser(description="a script to do stuff")
     parser.add_argument("--source", type=str, required=True)
@@ -11,6 +13,7 @@ def main():
     print("this is the main function")
     args = parse_args()
     print(args.source)
+    print(f'{os.getcwd()}'.join(DOCUPATH))
     pass
 
 if __name__ == "__main__":
